5dca65c4817cf5a7015946dfcccf662efd4f0efb
Philipp Sommer <philsommer2002@gmail.com>
2021-03-08 22:05:08 +0100

    added urls for api

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,9 @@
 from djongo import models
 
+class CommandModel(models.Model):
 
+
+class ComputerModel(models.Model):
+    name = models.CharField(blank=False,null=False)
+    ip = models.GenericIPAddressField(protocol="IPv4",blank=False,null=False)
+    
